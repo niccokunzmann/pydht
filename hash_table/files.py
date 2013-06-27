8546d7c6f03262e5147b21dfc9c0e9cdb457459d
Nicco Kunzmann <niccokunzmann@googlemail.com>
2013-06-27 21:00:21 +0200

    added hashing file

--- hash_table/files.py
+++ hash_table/files.py
@@ -1,7 +1,9 @@
 import io
 import tempfile
+from . import hashing
 
 class HashTableFileMixin:
+    """A mixin for a hashtable that adds itself to the hashtable when closed"""
 
     def __init__(self, hash_table, *args, **kw):
         super().__init__(*args, **kw)
@@ -26,9 +28,37 @@ class HashTableFileMixin:
             return super().__exit__(*args, **kw)
 
 class BytesIO(HashTableFileMixin, io.BytesIO):
+    """A io.BytesIO for a hashtable that adds itself to the hashtable when closed"""
     pass
 
 class SpooledTemporaryFile(HashTableFileMixin, tempfile.SpooledTemporaryFile):
+    """A tempfile.SpooledTemporaryFile for a hashtable that adds itself to the hashtable when closed"""
     pass
+
+class HashingFile:
+    """One can read from this file and the hash is updated"""
+
+    def __init__(self, file, length = None):
+        self._file = file
+        self._read = self._file.read
+        if hasattr(file, 'fileno'):
+            self.fileno = file.fileno
+        self._algorithm = hashing.algorithm()
+        self._length = length
+
+    def read(self, *args):
+        bytes = self._read(*args)
+        self._algorithm.update(bytes)
+        return bytes
+
+    @property
+    def hash(self):
+        return self._algorithm.hexdigest()
+
+    def __len__(self):
+        if self._length is None:
+            raise TypeError('length not supported for {}'.format(self._file))
+        return self._length
+        
 
 __all__ = ['BytesIO', 'SpooledTemporaryFile']
