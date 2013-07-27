7c7524a15d0cadbb2599558411930f78897f4ff2
Nicco Kunzmann <niccokunzmann@googlemail.com>
2013-07-27 16:20:17 +0200

    changed http mixin association

--- association_table/http_mixin.py
+++ association_table/http_mixin.py
@@ -1,37 +1,26 @@
 import os
 
 from .. import hashing
+from ..http.requester import Requester
 
 class HTTPMixin:
 
-    def __init__(self, hash_table, directory = None, *args, **kw):
-        super().__init__(hash_table, *args, **kw)
-        self._associations = {}
-        if directory is None:
-            directory = self.hash_table.get_base_directory()
-            if directory is None:
-                raise ValueError('I need a directory.')
-        self._directory = directory
-        self._hash_directory = hashing.HashDirectory(self._directory)
+    Requester = Requester
 
-    def _path_for_hash_and_index(self, hash, index):
-        directory = self._hash_directory.make_directory(hash, 'associations')
-        file_path = os.path.join(directory, str(index))
-        return file_path
+    def __init__(self, hash_table, base_url_or_requester = None):
+        super().__init__(hash_table)
+        if base_url_or_requester is None:
+            self._requester = self.Requester.make(hash_table)
+        else:
+            self._requester = self.Requester.make(base_url_or_requester)    
+        self._open_url = self._requester.open_url
 
-    def _add_association_at_index(self, index, hash, hashes_hash, hashes):
-        with open(self._path_for_hash_and_index(hash, index), 'a') as file:
-            file.write(hashes_hash)
-            file.write('\n')
+    def to_requester(self):
+        """=> this object's requester"""
+        return self._requester
 
-    def _get_association_hashes_at_index(self, index, hash):
-        path = self._path_for_hash_and_index(hash, index)
-        if not os.path.exists(path):
-            return set()
-        s = set()
-        with open(path, 'r') as file:
-            for line in file:
-                s.add(line.strip())
-        return s
+    def _find(self, association):
+        
+
     
 __all__ = ['HTTPMixin']
