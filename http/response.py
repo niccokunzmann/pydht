b0c8a3f2eabc9c3885be6451b96ca5e58896437b
Nicco Kunzmann <niccokunzmann@googlemail.com>
2013-07-27 17:17:01 +0200

    relocated method

--- http/response.py
+++ http/response.py
@@ -1,21 +1,20 @@
 from ..errors import *
 
 import http.client
-from ..association_table.association import Association
+
+def _method_for_status_code(name, doc):
+    def get_(self, *args, **kw):
+        code = self.getcode()
+        attribute = '_get_{}_{}'.format(name, code)
+        if hasattr(self, attribute):
+            return getattr(self, attribute)(*args, **kw)
+        raise UnexpectedStatusCode('Unexpected status code {} in get_{}'.format(code, name))
+    get_.__name__ += name
+    get_.__doc__ = doc
+    return get_
 
 class Response(http.client.HTTPResponse):
 
-    @staticmethod
-    def _method_for_status_code(name, doc):
-        def get_(self, *args, **kw):
-            code = self.getcode()
-            attribute = '_get_{}_{}'.format(name, code)
-            if hasattr(self, attribute):
-                return getattr(self, attribute)(*args, **kw)
-            raise UnexpectedStatusCode('Unexpected status code {} in get_{}'.format(code, name))
-        get_.__name__ += name
-        get_.__doc__ = doc
-        return get_
 
     get_file = _method_for_status_code('file', '=> the file object with the content')
 
@@ -25,7 +24,7 @@ class Response(http.client.HTTPResponse):
     def _get_file_200(self):
         return self
 
-    get_file = _method_for_status_code('size', '=> size of the request body')
+    get_size = _method_for_status_code('size', '=> size of the request body')
 
     def _get_size_404(self):
         return None
@@ -36,9 +35,12 @@ class Response(http.client.HTTPResponse):
             raise ContentLengthMissing('Response from {0} has no Content-Length'.format(request.geturl()))
         return int(size)
 
-    def get_associations(self):
+    def get_associations(self, association_class):
         file = self.get_file()
         associations = set()
         for line in file:
-            associations.add(Association.from_line(line))
+            associations.add(association_class.from_line(line))
         return associations
+
+__all__ = ['Response']
+
